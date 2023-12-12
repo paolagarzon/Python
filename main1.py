f45fcebed83e7f2342a195e210356eaf78e6c13d
paolagarzon <153647404+paolagarzon@users.noreply.github.com>
2023-12-12 11:25:42 -0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,33 @@
+#Ejercicio 1: Operaciones Basicas y Tipos de Datos
+a= 18
+b= 30
+c= a+b
+print(c)
+d= 37.55
+e= 9.63
+f= d-e
+print(f)
+g= 8
+h= 9
+i= g*h
+print(i)
+j= 158.46
+k=5.19
+l= j/k
+print(l)
+#Ejercicio 2: Manipulacion de Cadenas de Texto
+nombre= "Paola Michelle"
+apellido= " Garzon Ochoa"
+print("hola mi nombre es " +nombre+apellido)
+len("hola mi nombre es "+nombre+apellido)
+letras= len(nombre+apellido)
+print(letras)
+#Ejercicio 3: Conversion de Tipos de Datos
+m= 67
+n= 19.78
+float(m)
+int(n)
+print(m)
+print(n)
+print(float(m))
+print(int(m))
